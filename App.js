d74f9e8c8a8a10517dbf02a5c4075fbf633d8b3d
Marek C <you@example.com>
2021-12-28 18:52:43 -0500

    ADD: HomeScreen, NavOptions, Redux

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Provider } from "react-redux";
+import HomeScreen from "./screens/HomeScreen";
+import { store } from "./store";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+// setup redux
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		// View instead of div in React Native
+		// Provider - wrapper that injects
+		<Provider store={store}>
+			<SafeAreaProvider>
+				<HomeScreen />
+			</SafeAreaProvider>
+		</Provider>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
