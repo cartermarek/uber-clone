d1815de704fb2b08e69fec11f32b305bb47fd978
Marek C <you@example.com>
2021-12-29 08:51:31 -0500

    ADD: implement Google Autocomplete

--- App.js
+++ App.js
@@ -2,19 +2,43 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { Provider } from "react-redux";
 import HomeScreen from "./screens/HomeScreen";
+import MapScreen from "./screens/MapScreen";
 import { store } from "./store";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import "react-native-screens";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 // setup redux
 
 export default function App() {
+	const Stack = createNativeStackNavigator();
+
 	return (
 		// View instead of div in React Native
 		// Provider - wrapper that injects
 		<Provider store={store}>
-			<SafeAreaProvider>
-				<HomeScreen />
-			</SafeAreaProvider>
+			<NavigationContainer>
+				<SafeAreaProvider>
+					<Stack.Navigator>
+						<Stack.Screen
+							name="HomeScreen"
+							component={HomeScreen}
+							options={{
+								headerShown: false,
+							}}
+						/>
+						<Stack.Screen
+							name="MapScreen"
+							component={MapScreen}
+							options={{
+								headerShown: false,
+							}}
+						/>
+					</Stack.Navigator>
+					{/* <HomeScreen /> */}
+				</SafeAreaProvider>
+			</NavigationContainer>
 		</Provider>
 	);
 }
